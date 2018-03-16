f3ca7797353837629667a85734a4f720e82b72eb
feverdestiny <feverdestiny@163.com>
2018-03-16 18:14:00 +0800

    添加路由

--- src/router/index.js
+++ src/router/index.js
@@ -4,7 +4,8 @@ import Router from "vue-router";
  * 统一导入vue文件格式
  * @param {*} file  文件名
  */
-const _resolve = file => resolve => require([`@/components/${file}.vue`], resolve);
+const _resolve = file => resolve =>
+  require([`@/components/${file}.vue`], resolve);
 
 Vue.use(Router);
 
@@ -21,28 +22,53 @@ export default new Router({
           children: [
             {
               path: "/",
-              redirect: '/discover'
-            }, {
+              redirect: "/discover"
+            },
+            {
               path: "/discover",
               name: "推荐",
               component: _resolve("views/Discover/recommend/index")
-            }, {
+            },
+            {
               path: "/discover/toplist",
               name: "toplist",
               component: _resolve("views/Discover/toplist/index")
+            },
+            {
+              path: "/discover/playlist",
+              name: "playlist",
+              component: _resolve("views/Discover/playlist/index")
+            },
+            {
+              path: "/discover/djradio",
+              name: "djradio",
+              component: _resolve("views/Discover/djradio/index")
+            },
+            {
+              path: "/discover/artist",
+              name: "artist",
+              component: _resolve("views/Discover/artist/index")
+            },
+            {
+              path: "/discover/album",
+              name: "album",
+              component: _resolve("views/Discover/album/index")
             }
           ]
-        }, {
+        },
+        {
           path: "/my",
           name: "我的音乐",
           component: _resolve("views/My/index")
-        }, {
+        },
+        {
           path: "/icon",
           name: "Icon",
           component: _resolve("views/iconDemo/index")
         }
       ]
-    }, {
+    },
+    {
       path: "/icon",
       name: "Icon",
       component: _resolve("views/iconDemo/index")
