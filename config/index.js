172238427156080c01419e20bdca6256be96c3d0
feverdestiny <feverdestiny@163.com>
2018-03-21 18:10:48 +0800

    配置代理

--- config/index.js
+++ config/index.js
@@ -21,6 +21,7 @@ module.exports = {
 
     // Various Dev Server settings
     host: "localhost", // can be overwritten by process.env.HOST
+    // host: "10.139.133.169", // can be overwritten by process.env.HOST
     port: 8891, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
     autoOpenBrowser: false,
     errorOverlay: true,
