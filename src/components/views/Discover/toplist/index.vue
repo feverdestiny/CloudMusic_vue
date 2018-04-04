c831519b045b7d3d21ac24cf1803ceeaad927b40
feverdestiny <feverdestiny@163.com>
2018-04-04 23:32:05 +0800

    添加排行榜

--- src/components/views/Discover/toplist/index.vue
+++ src/components/views/Discover/toplist/index.vue
@@ -1,39 +1,100 @@
 <template>
-    <div class="m-toplist">
+    <div class="m-toplist f-cb">
         <div class="list-left">
             <div class="le-main">
                 <h2>
                     云音乐特色榜
                 </h2>
                 <ul class="f-cb">
-                    <li>
+                    <li v-for="(item,index) in featureList" :key="index">
                         <div class="item f-cb">
                             <div class="left">
                                 <a class="avatar">
-                                    <img src="http://p1.music.126.net/N2HO5xfYEqyQ8q6oxCw8IQ==/18713687906568048.jpg?param=40y40" />
-                                    <span class="msk"></span>
+                                    <img :src='item.url' />
+                                    <!-- <span class="msk"></span> -->
                                 </a>
                             </div>
                             <div class="name">
-                                <a class="nam1">云音乐飙升榜</a>
+                                <a class="nam1">{{item.name}}</a>
                             </div>
-                            <div class="ntime">每天更新</div>
+                            <div class="ntime">{{item.time}}</div>
+                        </div>
+                    </li>
+                </ul>
+                  <h2 style="margin-top:20px;">
+                    全球媒体榜
+                </h2>
+                 <ul class="f-cb" >
+                    <li v-for="(item,index) in globalList" :key="index">
+                        <div class="item f-cb">
+                            <div class="left">
+                                <a class="avatar">
+                                    <img :src='item.url' />
+                                    <!-- <span class="msk"></span> -->
+                                </a>
+                            </div>
+                            <div class="name">
+                                <a class="nam1">{{item.name}}</a>
+                            </div>
+                            <div class="ntime">{{item.time}}</div>
                         </div>
                     </li>
                 </ul>
             </div>
         </div>
         <div class="list-right">
-            2
+            <div class="right-wrap">
+              <div class="wrap-info f-cb">
+                <div class="cover">
+                  <img src='http://p1.music.126.net/DrRIg6CrgDfVLEph9SNh7w==/18696095720518497.jpg?param=150y150'/>
+               <span class="msk"></span>
+                </div>
+                <div class="cnt">
+                  <div class="cntc ">
+                    <div class="dh f-cb">
+                      <h2>云音乐飙升榜</h2>
+                    </div>
+                    <div class="user f-cb">
+                      <i class="u-icon"></i>
+                      <span class="sep">最近更新：04月04日</span>
+                      <span class="ss">（每天更新）</span>
+                    </div>
+                    <div class="btn f-cb">
+<div class="btn1">
+  <i>
+    <em class="ply"></em>
+    播放
+  </i>
+</div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
         </div>
 
     </div>
 </template>
+<script>
+import { feature, global } from "./list.js";
+export default {
+  data() {
+    return {
+      featureList: feature,
+      globalList: global
+    };
+  }
+};
+</script>
+
 
 
 <style lang="scss">
 $wrapBg: url("../../../../assets/images/wrap3.png")repeat-y center 0;
 $coverBg: url("../../../../assets/images/coverall.png")no-repeat;
+$iconBg: url("../../../../assets/images/icon.png")no-repeat;
+$tbnBg: url("../../../../assets/images/button2.png")no-repeat 0 9999px;
+
 .m-toplist {
   font-size: 12px;
   width: 980px;
@@ -116,6 +177,122 @@ $coverBg: url("../../../../assets/images/coverall.png")no-repeat;
     float: right;
     width: 740px;
     padding-bottom: 50px;
+    .right-wrap {
+      padding: 40px;
+      .wrap-info {
+        .cover {
+          float: left;
+          position: relative;
+          display: inline;
+          margin: 0 -220px 0 0;
+          padding: 3px;
+          border: 1px solid #ccc;
+          width: 150px;
+          height: 150px;
+          img {
+            width: 150px;
+            height: 150px;
+          }
+          .msk {
+            position: absolute;
+            width: 150px;
+            height: 150px;
+            background: $coverBg;
+            background-position: -230px -380px;
+            top: 3px;
+            left: 3px;
+          }
+        }
+        .cnt {
+          float: right;
+          width: 100%;
+          .cntc {
+            margin-left: 187px;
+            .dh {
+              margin: 16px 0 4px;
+              position: relative;
+              line-height: 24px;
+              h2 {
+                line-height: 24px;
+                font-size: 20px;
+                font-weight: normal;
+                font-family: "Microsoft Yahei", Arial, Helvetica, sans-serif;
+              }
+            }
+            .user {
+              margin: 0 0 20px;
+              line-height: 35px;
+              i {
+                font-style: normal;
+                text-align: left;
+                font-size: inherit;
+              }
+              .u-icon {
+                float: left;
+                display: inline-block;
+                overflow: hidden;
+                vertical-align: middle;
+                margin: 9px 0 0 3px;
+                width: 13px;
+                height: 13px;
+                background: $iconBg;
+                background-position: -18px -682px;
+              }
+              .sep {
+                margin-left: 5px;
+                color: #666;
+              }
+              .ss {
+                color: #999;
+              }
+            }
+            .btn {
+              margin-bottom: 25px;
+              margin-right: -10px;
+              .btn1 {
+                float: left;
+                color: #fff;
+                background: $tbnBg;
+                background-position: right -428px;
+                text-decoration: none;
+                padding: 0 5px 0 0;
+                white-space: nowrap;
+
+                i {
+                  font-style: normal;
+                  text-align: left;
+                  padding: 0 7px 0 8px;
+                  font-size: inherit;
+                  background: $tbnBg;
+                  background-position: 0 -387px;
+                  pointer-events: none;
+                  display: inline-block;
+                  height: 31px;
+                  line-height: 31px;
+                  overflow: hidden;
+                  vertical-align: top;
+                  text-align: center;
+                  cursor: pointer;
+
+                  .ply {
+                    font-style: normal;
+                    text-align: left;
+                    font-size: inherit;
+                    float: left;
+                    width: 20px;
+                    height: 18px;
+                    margin: 6px 2px 2px 0;
+                    background: $tbnBg;
+                    background-position: 0 -1622px;
+                    overflow: hidden;
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
   }
 }
 </style>
---
+++ src/components/views/Discover/toplist/list.js
@@ -0,0 +1,148 @@
+export const feature = [
+  {
+    index: 1,
+    name: "云音乐飙升榜",
+    url: require("../../../../assets/images/bangd/bsb.jpg"),
+    time: "每天更新"
+  },
+  {
+    index: 2,
+    name: "云音乐新歌榜",
+    url: require("../../../../assets/images/bangd/xgb.jpg"),
+    time: "每天更新"
+  },
+  {
+    index: 3,
+    name: "网易原创歌曲榜",
+    url: require("../../../../assets/images/bangd/ycgqb.jpg"),
+    time: "每周四更新"
+  },
+  {
+    index: 4,
+    name: "云音乐热歌榜",
+    url: require("../../../../assets/images/bangd/rgb.jpg"),
+    time: "每周四更新"
+  }
+];
+export const global = [
+  {
+    index: 1,
+    name: "云音乐电音榜",
+    url: require("../../../../assets/images/bangd/dyb.jpg"),
+    time: "每周五更新"
+  },
+  {
+    index: 2,
+    name: "云音乐嘻哈榜",
+    url: require("../../../../assets/images/bangd/xhb.jpg"),
+    time: "每周五更新"
+  },
+  {
+    index: 3,
+    name: "英国Q杂志中文版周榜",
+    url: require("../../../../assets/images/bangd/ygzwb.jpg"),
+    time: "刚刚更新"
+  },
+  {
+    index: 4,
+    name: "电竞音乐榜",
+    url: require("../../../../assets/images/bangd/djb.jpg"),
+    time: "每周五更新"
+  },
+  {
+    index: 5,
+    name: "云音乐新电力榜",
+    url: require("../../../../assets/images/bangd/xdlb.jpg"),
+    time: "每周五更新"
+  },
+  {
+    index: 6,
+    name: "云音乐ACG音乐榜",
+    url: require("../../../../assets/images/bangd/acgb.jpg"),
+    time: "每周四更新"
+  },
+  {
+    index: 7,
+    name: "云音乐古典音乐榜",
+    url: require("../../../../assets/images/bangd/gdyyb.jpg"),
+    time: "每周四更新"
+  },
+  {
+    index: 8,
+    name: "UK排行榜",
+    url: require("../../../../assets/images/bangd/ukb.jpg"),
+    time: "每周一更新"
+  },
+  {
+    index: 9,
+    name: "美国Billboard周榜",
+    url: require("../../../../assets/images/bangd/billboard.jpg"),
+    time: "刚刚更新"
+  },
+  {
+    index: 10,
+    name: "Beatport全球电子舞歌曲",
+    url: require("../../../../assets/images/bangd/beatport.jpg"),
+    time: "每周四更新"
+  },
+  {
+    index: 11,
+    name: "法国NRJ Vos Hits 周榜",
+    url: require("../../../../assets/images/bangd/nrj.jpg"),
+    time: "每周五更新"
+  },
+  {
+    index: 12,
+    name: "KTV唛榜",
+    url: require("../../../../assets/images/bangd/ktv.jpg"),
+    time: "每周五更新"
+  },
+  {
+    index: 13,
+    name: "iTunes榜",
+    url: require("../../../../assets/images/bangd/itunes.jpg"),
+    time: "每周五更新"
+  },
+  {
+    index: 14,
+    name: "日本Oricon周榜",
+    url: require("../../../../assets/images/bangd/oricon.jpg"),
+    time: "每周三更新"
+  },
+  {
+    index: 15,
+    name: "Hit FM Top榜",
+    url: require("../../../../assets/images/bangd/hitfm.jpg"),
+    time: "每周一更新"
+  },
+  {
+    index: 16,
+    name: "台湾Hito排行榜",
+    url: require("../../../../assets/images/bangd/hito.jpg"),
+    time: "每周一更新"
+  },
+  {
+    index: 17,
+    name: "中国TOP排行榜(港台榜)",
+    url: require("../../../../assets/images/bangd/gtb.jpg"),
+    time: "每周一更新"
+  },
+  {
+    index: 18,
+    name: "中国TOP排行榜(内地榜)",
+    url: require("../../../../assets/images/bangd/ndb.jpg"),
+    time: "每周一更新"
+  },
+  {
+    index: 19,
+    name: "香港电台中文歌曲龙虎榜",
+    url: require("../../../../assets/images/bangd/xgdt.jpg"),
+    time: "每周五更新"
+  },
+  {
+    index: 20,
+    name: "中国嘻哈榜",
+    url: require("../../../../assets/images/bangd/zgxhb.jpg"),
+    time: "每周五更新"
+  }
+];
