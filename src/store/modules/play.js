00417c58953197014d00c743c8782ce417bb1f63
feverdestiny <feverdestiny@163.com>
2018-04-24 17:39:34 +0800

    重构调用结构

--- src/store/modules/play.js
+++ src/store/modules/play.js
@@ -1,25 +1,29 @@
+import axios from "@/plug/axios/index";
+
 const play = {
-    state: {
-        artist: {},
-        song: {}
+  state: {
+    artist: {},
+    song: {}
+  },
+  actions: {
+    setArtist({ commit }, artist) {
+      commit("SET_ARTIST", artist);
     },
-    actions: {
-        setArtist({commit},artist) {
-            commit('SET_ARTIST',artist)
-        },
-        setSong({commit},song) {
-            commit('SET_SONG',song)
-        },
-    },
-    mutations: {
-        SET_ARTIST: (state, artist) => {
-            state.artist = artist
-        },
-        SET_SONG: (state, song) => {
-            state.song = song
-        }
-
+    async setSong({ commit }, id) {
+      const Res = await axios.get(`/song/detail?ids=${id}`);
+      if (Res.code === 200) {
+        commit("SET_SONG", Res.songs[0]);
+      }
     }
-}
+  },
+  mutations: {
+    SET_ARTIST: (state, artist) => {
+      state.artist = artist;
+    },
+    SET_SONG: (state, song) => {
+      state.song = song;
+    }
+  }
+};
 
-export default play
+export default play;
